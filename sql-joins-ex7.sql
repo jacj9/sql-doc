292948f59d39670d5e2a0fcbab5209234f4477ea
jacj9 <jjoapr@gmail.com>
2025-01-11 09:50:58 -0800

    Create sql-joins-ex7.sql

---
+++ sql-joins-ex7.sql
@@ -0,0 +1,15 @@
+--  7. From the following table, write a SQL query to find the employees who earn less than the employee of ID 182. Return first name, last name and salary.
+
+SELECT e1.first_name, e1.last_name, e1.salary
+FROM employees e1
+JOIN employees e2 ON e1.employee_id = e2.employee_id
+WHERE e1.salary <
+(SELECT e2.salary
+  FROM employees e2
+  WHERE e2.employee_id = 182);
+
+-- Alternative solution:
+SELECT e.first_name, e.last_name, e.salary
+FROM employees e
+JOIN employees s ON e.salary < s.salary
+AND s.employee_id = 182;
