77b9380ce37e8e0c470ad709e36f3ee8c7bf32ed
jacj9 <jjoapr@gmail.com>
2025-01-16 22:01:15 -0800

    Create sql_subq_ex48.sql

---
+++ sql_subq_ex48.sql
@@ -0,0 +1,2 @@
+-- 48. From the following tables, write a SQL query to find those departments where maximum salary is 7000 and above. The employees worked in those departments have already completed one or more jobs. Return all the fields of the departments.
+
