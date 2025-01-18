b0f1e15c0bf2ebbbeff3edb6f59dd6376b855968
jacj9 <jjoapr@gmail.com>
2025-01-17 16:42:37 -0800

    Update sql_joins_ex12.sql

--- sql_joins_ex12.sql
+++ sql_joins_ex12.sql
@@ -1,5 +1,6 @@
 -- 12. From the following tables, write a SQL query to find the employees who work in the same department as the employee with the last name Taylor. Return first name, last name and department ID.
 
+-- My initial solution:
 SELECT first_name, last_name, department_id
 FROM employees
 WHERE department_id IN
@@ -7,6 +8,7 @@ WHERE department_id IN
 FROM employees
 WHERE last_name = 'Taylor');
 
+-- Second attempt:
 SELECT A.first_name, A.last_name, A.department_id
 FROM employees A
 JOIN employees B ON A.department_id = B.department_id AND B.last_name = 'Taylor';
