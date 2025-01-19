aad6e3cae3095a70d9136bfbd103c39503c3a7d4
jacj9 <jjoapr@gmail.com>
2025-01-19 07:14:39 -0800

    Update sql_subq_ex49.sql

--- sql_subq_ex49.sql
+++ sql_subq_ex49.sql
@@ -1,2 +1,9 @@
 -- 49. From the following tables, write a SQL query to find those departments where the starting salary is at least 8000. Return all the fields of departments.
 
+SELECT *
+FROM departments
+WHERE department_id IN
+(SELECT department_id
+FROM employees
+GROUP by department_id
+HAVING MIN(salary) >= 8000);
