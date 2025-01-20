1576a5abd3cf7bfe1ea31403e913ec5991aa3bf1
jacj9 <jjoapr@gmail.com>
2025-01-20 07:45:57 -0800

    Update sql_joins_ex14.sql

--- sql_joins_ex14.sql
+++ sql_joins_ex14.sql
@@ -5,3 +5,12 @@ SELECT j.job_title,
   j.max_salary - e.salary AS salary_difference
 FROM employees e
 JOIN jobs j ON e.job_id = j.job_id;
+
+-- Other Solution:
+-- Selecting specific columns (job_title, first_name || ' ' || last_name AS Employee_name, max_salary - salary AS salary_difference) from the 'employees' table
+SELECT job_title, first_name || ' ' || last_name AS Employee_name, max_salary - salary AS salary_difference 
+
+-- Performing a NATURAL JOIN between the 'employees' table and the 'jobs' table, where the join is implicitly based on columns with the same name in both tables
+FROM employees 
+
+NATURAL JOIN jobs;
