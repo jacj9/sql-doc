7a8a848ec54dbce86ffe5f7aeb898fc7f6f35297
jacj9 <jjoapr@gmail.com>
2025-01-21 16:54:31 -0800

    Update sql_subq_ex51.sql

--- sql_subq_ex51.sql
+++ sql_subq_ex51.sql
@@ -1,5 +1,17 @@
 -- From the following table, write a SQL query to find employees who have previously worked as 'Sales Representatives'. Return all the fields of jobs.
 
+-- Initial solution:
+SELECT *
+FROM jobs
+WHERE job_id =
+(SELECT job_id
+FROM employees
+WHERE employee_id IN
+(SELECT employee_id
+FROM job_history
+WHERE job_id = 'SA_REP'));
+
+-- Sample solution, and with the same output:
 SELECT *
 FROM jobs
 WHERE job_id IN
