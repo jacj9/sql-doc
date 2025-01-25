9b831a2c8c412bddc3f6d7a3a5f47d7d9bbab564
jacj9 <jjoapr@gmail.com>
2025-01-25 09:07:54 -0800

    Update sql_subq_ex54.sql

--- sql_subq_ex54.sql
+++ sql_subq_ex54.sql
@@ -1,5 +1,6 @@
 -- 54. From the following table, write a  SQL query to find those employees who earn the highest salary in a department. Return department ID, employee name, and salary.
 
+-- First attempt:
 SELECT department_id, 
 first_name || ' ' || last_name AS employee_name, salary
 FROM employees e
@@ -7,3 +8,13 @@ WHERE salary =
 (SELECT MAX(salary)
 FROM employees
 WHERE department_id = e.department_id);
+
+-- Second and final attempt:
+SELECT e.department_id, 
+e.first_name || ' ' || e.last_name AS employee_name, 
+e.salary
+FROM employees e
+WHERE e.salary IN
+(SELECT MAX(salary)
+FROM employees
+WHERE e.department_id = department_id);
