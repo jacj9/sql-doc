fd18c5977013b513f895a695c25cdf9d6eecd493
jacj9 <jjoapr@gmail.com>
2025-01-25 09:14:16 -0800

    Update sql_joins_ex18.sql

--- sql_joins_ex18.sql
+++ sql_joins_ex18.sql
@@ -1,6 +1,13 @@
 -- From the following tables, write a  SQL query to find the department name and the full name (first and last name) of the manager.
 
--- First solution try did not retrieve the correct output:
+-- First attempt did not retrieve the correct output:
 SELECT d.department_name, e.first_name || ' ' || e.last_name name_of_manager
 FROM departments d
 JOIN employees e ON d.manager_id = e.employee_id;
+
+-- Second attempt:
+SELECT d.department_name, 
+e.first_name || ' ' || e.last_name AS name_of_manager
+FROM departments d
+JOIN employees e 
+  ON d.manager_id = e.employee_id;
