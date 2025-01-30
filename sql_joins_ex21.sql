c001458d3f2abb9aaca44da28fcf4a7ccce83bc6
jacj9 <jjoapr@gmail.com>
2025-01-29 16:29:08 -0800

    Update sql_joins_ex21.sql

--- sql_joins_ex21.sql
+++ sql_joins_ex21.sql
@@ -7,3 +7,28 @@ JOIN locations L ON C.country_id = L.country_id
 JOIN departments D ON L.location_id = D.location_id
 JOIN employees E ON D.department_id = E.department_id
 GROUP BY C.country_name, L.city;
+
+
+-- Second attempt: This one makes more sense
+SELECT country_name, city, department_id
+FROM countries
+JOIN locations USING (country_id)
+JOIN departments USING (location_id)
+WHERE department_id IN
+(SELECT department_id
+FROM employees
+GROUP BY department_id
+HAVING COUNT(employee_id)>=2);
+
+
+-- Final attempt
+SELECT country_name, city, COUNT(department_id) AS num_employee
+FROM countries
+JOIN locations USING (country_id)
+JOIN departments USING (location_id)
+WHERE department_id IN
+(SELECT department_id
+FROM employees
+GROUP BY department_id
+HAVING COUNT(employee_id)>=2)
+GROUP BY country_name, city;
