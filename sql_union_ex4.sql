c5b4ac08849c0b1aca1b806e33afaa6c27bb1934
jacj9 <jjoapr@gmail.com>
2025-02-01 10:13:03 -0800

    Update sql_union_ex4.sql

--- sql_union_ex4.sql
+++ sql_union_ex4.sql
@@ -7,3 +7,18 @@ UNION
 (SELECT salesman_id, ord_no, MAX(ord_no), MIN(ord_no), ord_date
 FROM orders
 GROUP BY salesman_id);
+
+-- Second attempt:
+SELECT S.salesman_id, S.name, A.ord_no, 'highest on ', A.ord_date
+FROM salesman S, orders A
+WHERE A.purch_amt =
+(SELECT MAX(B.purch_amt)
+FROM orders B
+WHERE A.ord_no = B.ord_no)
+UNION
+SELECT X.salesman_id, X.name, C.ord_no, 'lowerst on', C.ord_date
+FROM salesman X, orders C
+WHERE C.purch_amt =
+(SELECT MIN(D.purch_amt)
+FROM orders D
+WHERE C.ord_no = D.ord_no);
