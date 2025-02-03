dd872d04c728b956bf25898227cb320d32a6f46f
jacj9 <jjoapr@gmail.com>
2025-02-02 20:41:14 -0800

    Update sql_joins_ex23.sql

--- sql_joins_ex23.sql
+++ sql_joins_ex23.sql
@@ -4,3 +4,9 @@ SELECT H.employee_id, J.job_title, (H.end_date - H.start_date) AS "days"
 FROM jobs J
 JOIN job_history H ON J.job_id = H.job_id
 WHERE H.department_id = 80;
+
+-- Another attempt:
+SELECT employee_id, job_title, end_date - start_date AS days
+FROM jobs
+NATURAL JOIN job_history
+WHERE department_id = 80;
