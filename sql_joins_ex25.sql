58d61ecbb077df7d38753b640df4dfda3180218d
jacj9 <jjoapr@gmail.com>
2025-02-05 22:00:40 -0800

    Update sql_joins_ex25.sql

--- sql_joins_ex25.sql
+++ sql_joins_ex25.sql
@@ -7,3 +7,16 @@ JOIN employees e USING (job_id)
 JOIN job_history jh USING (employee_id)
 WHERE e.commission_pct = 0;
 
+-- Second attempt:
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) full_name, 
+  j.job_title, 
+  h.starting_date, 
+  h.ending_date, 
+  e.employee_id
+FROM employees e
+JOIN (SELECT MAX(start_date)starting_date, MAX(end_date)ending_date, employee_id
+        FROM  job_history 
+        GROUP BY employee_id) h ON e.employee_id = h.employee_id
+JOIN jobs j ON e.job_id = j.job_id
+WHERE e.commission_pct = 0;
