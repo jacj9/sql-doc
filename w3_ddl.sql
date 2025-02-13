c285f5a44839d1d0b2cff16b941bbce5a36c5d68
jacj9 <jjoapr@gmail.com>
2025-02-12 21:19:50 -0800

    Update w3_ddl.sql

--- w3_ddl.sql
+++ w3_ddl.sql
@@ -13,7 +13,8 @@ date_release DATETIME NOT NULL
 ALTER TABLE employees
 ADD COLUMN birthdate DATETIME NOT NULL,
 ADD COLUMN hire_date DATETIME NOT NULL,
-ADD COLUMN department VARCHAR(50);
+ADD COLUMN department VARCHAR(50)
+ADD COLUMN salary INT;
 
 ALTER TABLE customers
 ADD COLUMN created_at DATETIME,
@@ -21,3 +22,13 @@ ADD COLUMN phone_number INT,
 ADD COLUMN is_active BOOLEAN,
 ADD COLUMN discount_rate DECIMAL;
 
+
+-- 3. Modify a Column's Data Type: Write a SQL query to change the data type of an existing column in a table.
+ALTER TABLE customers
+MODIFY COLUMN phone_nuber SMALLINT;
+
+ALTER TABLE employees
+-- Modify from INT to DECIMAL(10,2)
+MODIFY COLUMN salary DECIMAL(10,2), 
+MODIFY COLUMN department TEXT,
+MODIFY COLUMN birthday DATE; 
