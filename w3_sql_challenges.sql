b5f5d9fe4728b6754798a0515b593a71b4fa4b00
jacj9 <jjoapr@gmail.com>
2025-02-27 18:36:15 -0800

    Update w3_sql_challenges.sql

--- w3_sql_challenges.sql
+++ w3_sql_challenges.sql
@@ -1,1 +1,1 @@
-"""Documenting my sql exercises in this document."""
+"""Documenting my sql exercises in this document. The sql challenge exercises are from https://www.w3resource.com/sql-exercises/challenges-1/index.php"""
