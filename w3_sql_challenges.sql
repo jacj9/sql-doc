5ce63067c7185ffbefd948adb49646432dd7d096
jacj9 <jjoapr@gmail.com>
2025-02-28 23:41:39 -0800

    Update w3_sql_challenges.sql

--- w3_sql_challenges.sql
+++ w3_sql_challenges.sql
@@ -1,1 +1,7 @@
 """Documenting my sql exercises in this document. The sql challenge exercises are from https://www.w3resource.com/sql-exercises/challenges-1/index.php"""
+
+-- 1. From the following tables, write a SQL query to find the information on each salesperson of ABC Company. Return name, city, country and state of each salesperson.
+
+SELECT first_name, last_name, city, state, country
+FROM salesperson a
+LEFT JOIN address ON a.salesman_id = b.salesman_id;
