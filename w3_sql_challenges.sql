6dbcc40487e3f15475724392ec18dfdf94997310
jacj9 <jjoapr@gmail.com>
2025-03-02 13:33:56 -0800

    Update w3_sql_challenges.sql

--- w3_sql_challenges.sql
+++ w3_sql_challenges.sql
@@ -1,7 +1,26 @@
-"""Documenting my sql exercises in this document. The sql challenge exercises are from https://www.w3resource.com/sql-exercises/challenges-1/index.php"""
+"""Practicing sql exercises in this document. The sql challenge exercises are from https://www.w3resource.com/sql-exercises/challenges-1/index.php"""
 
 -- 1. From the following tables, write a SQL query to find the information on each salesperson of ABC Company. Return name, city, country and state of each salesperson.
 
 SELECT first_name, last_name, city, state, country
 FROM salesperson a
 LEFT JOIN address ON a.salesperson_id = b.salesperson_id;
+
+
+-- 2. From the following table, write a SQL query to find the third highest sale. Return sale amount.
+
+SELECT MAX(sale_amt) AS ThirdHighestSale
+FROM salemast
+WHERE sale_amt <
+(
+  SELECT MAX(sale_amt)
+  FROM salemast
+  WHERE sale_amt <
+    (
+    SELECT MAX(sale_amt)
+    FROM salemast
+    )
+);
+
+
+-- Retry exercise 2 using LIMIT OFFSET
