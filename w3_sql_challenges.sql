63ffe6495a2edd6fab374c93a871c129b16b6096
jacj9 <jjoapr@gmail.com>
2025-03-04 22:03:28 -0800

    Update w3_sql_challenges.sql

--- w3_sql_challenges.sql
+++ w3_sql_challenges.sql
@@ -27,3 +27,21 @@ WHERE sale_amt <
 SELECT MAX(sale_amt) AS ThirdHighestSale
 FROM salemast
 LIMIT 1 OFFSET 2; -- Display one value starting from third value
+
+
+-- 3. From the following table, write a SQL query to find the Nth highest sale. Return sale amount.
+
+SELECT MAX(sale_amt) AS getNthHighestSaleAmt
+FROM salemast;
+
+
+-- 4. From the following table, write a SQL query to find the marks, which appear at least thrice one after another without interruption. Return the number.
+
+SELECT DISTINCT mark
+FROM (
+  SELECT mark,
+          LAG(mark) OVER (ORDER BY id) AS prev_mark,
+          LEAD(mark) OVER (ORDER BY id) AS next_mark
+  FROM marks_table
+  ) subquery
+WHERE mark = prev_mark AND MARK = next_nark;
